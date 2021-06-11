8c19ddb2f62762df98bbb25d2bf2d44043ac13e9
Alexandra <arbeuerman@gmail.com>
2021-06-11 09:21:30 -0500

    getting search bar to work

--- src/App.js
+++ src/App.js
@@ -2,25 +2,38 @@ import './App.css';
 import { Component } from 'react';
 import ActivityContainer from './components/ActivityContainer';
 import Header from './components/Header';
+import SearchBar from './components/SearchBar'
 
 const activitiesUrl = 'http://localhost:3000/activities'
 
 class App extends Component {
 
   state = {
-    activities: []
+    activities: [],
+    searchText: ''
   }
 
   componentDidMount() {
+    this.getAllActivities();
+  }
+
+  getAllActivities = () => {
     fetch(activitiesUrl)
     .then(res => res.json())
     .then(activities => this.setState({activities}))
+  }
+
+  handleSearch = (searchText) => {
+    const filteredActivities = [...this.state.activities].filter(activity => activity.category.includes(searchText))
+    // console.log(filteredActivities)
+    this.setState({activities: filteredActivities})
   }
 
   render(){
     return (
       <div className="App">
         <Header /> 
+        <SearchBar handleSearch={this.handleSearch}/>
         <ActivityContainer activities={this.state.activities}/>
       </div>
     );
---
+++ src/components/SearchBar.js
@@ -0,0 +1,38 @@
+import Form from 'react-bootstrap/Form'
+import FormControl from 'react-bootstrap/FormControl'
+import Button from 'react-bootstrap/Button'
+import { Component } from "react";
+
+class SearchBar extends Component{
+
+    state = {
+        searchText: ''
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        this.props.handleSearch(this.state.searchText)
+    }
+
+    render(){
+        return (
+            <div style={{marginLeft: '15px'}}>
+                <Form as='form' noValidate inline onSubmit={this.handleSubmit}>
+                    <FormControl 
+                        type="text" 
+                        placeholder="Search" 
+                        className="mr-sm-2" 
+                        value={this.state.searchText}
+                        onChange={(event) => {this.setState({searchText: event.target.value})}}
+                    />
+                    <Button 
+                        variant="outline-success" 
+                        type='submit'
+                    >Search</Button>
+                </Form>
+            </div>
+        )
+    }
+}
+
+export default SearchBar;

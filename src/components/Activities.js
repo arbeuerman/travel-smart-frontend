0d1de99191caa448caed99b90c09d8b12c0399f6
Alexandra <arbeuerman@gmail.com>
2021-06-19 14:32:08 -0500

    added new component to remove duplication from profile and all activities viewing

---
+++ src/components/Activities.js
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+import ActivityInfo from "./ActivityInfo";
+import ActivityCard from "./ActivityCard";
+import CardDeck from "react-bootstrap/CardDeck";
+
+
+function Activities(props) {
+    const [activity, setActivity] = useState({})
+    const [showActivityInfo, setShowActivityInfo] = useState(false)
+
+    const displaySingleCard = (activity) => {
+        setShowActivityInfo(true)
+        setActivity(activity)
+    }
+
+    const displayAllActivities = () => {
+        setShowActivityInfo(false)
+    }
+
+    return(
+        <div style={{marginTop: '5px'}}>
+            { showActivityInfo 
+                ? <ActivityInfo 
+                    activity={activity} 
+                    location={props.location} 
+                    displayAllActivities={displayAllActivities}/>
+                : <CardDeck>
+                        {props.activities.map(activity => 
+                            <ActivityCard 
+                                key={activity.id} 
+                                activity={activity} 
+                                displaySingleCard={displaySingleCard}
+                            />)}
+                  </CardDeck>
+            }
+        </div>
+    )
+}
+
+export default Activities;
